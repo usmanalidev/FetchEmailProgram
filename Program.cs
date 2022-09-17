de8794ab831f26dd726b5e3fa306a1c9f2b43fc4
Usman Ali <usmanali.dev@gmail.com>
2022-09-17 12:38:19 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FetchEmailProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
